82fcdbf993d5cd5521e92b8a00cb158c121c1544
Lindrit Krasniqi <linndritks@gmail.com>
2021-09-03 15:27:07 +0200

    Create user model

---
+++ models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+module.exports = mongoose.model(
+  "User",
+  new mongoose.Schema({
+    name: {
+      required: true,
+      type: String,
+    },
+  })
+);
