d0385963082be6b9d760a9ede29ce5f430912224
Lindrit Krasniqi <linndritks@gmail.com>
2021-09-07 14:51:55 +0200

    Define user model with more details

--- models/user.js
+++ models/user.js
@@ -1,11 +1,14 @@
-const mongoose = require("mongoose");
+const mongoose = require("../database");
 
 module.exports = mongoose.model(
   "User",
   new mongoose.Schema({
-    name: {
-      required: true,
-      type: String,
-    },
+    name: { required: true, type: String },
+    username: { required: true, type: String, unique: true },
+    email: { required: true, type: String, unique: true },
+    password: { required: true, type: String },
+    email_verified_at: { required: false, type: Date, default: null },
+    created_at: { required: true, type: Date, default: Date.now() },
+    updated_at: { required: true, type: Date, default: Date.now() },
   })
 );
