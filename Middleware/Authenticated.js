8d4f0aa7c04d66bd1178e1142b97df3bf7ec1697
Lindrit Krasniqi <linndritks@gmail.com>
2021-09-08 16:32:29 +0200

    Set user on request object and exclude password field

--- Middleware/Authenticated.js
+++ Middleware/Authenticated.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const User = require("../Models/User");
 
 const requireAuth = (req, res, next) => {
   const token =
@@ -13,12 +14,12 @@ const requireAuth = (req, res, next) => {
 
 const verifyToken = async (req, res, next) => {
   const token =
-    req.body.token || req.query.token || req.headers["jwt"];
+    req.body.token || req.query.token || req.headers["Authorization"];
 
   try {
     const decoded = await jwt.verify(token, process.env.JWT_SECRET);
 
-    req.user = decoded;
+    req.user = await User.findOne({ _id: decoded._id }).select("-password");
   } catch (error) {
     return res.status(401).json({ message: "Unauthenticated." });
   }
