36d8912cad735374a446c0260be49055e1510749
Lindrit Krasniqi <linndritks@gmail.com>
2021-09-08 16:35:39 +0200

    Register me route to fetch current user

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,22 @@
 const router = require("express").Router();
+
+// import Middlewares
 const { requireAuth, verifyToken } = require("../Middleware/Authenticated");
+
+// import Validations
 const LoginValidation = require("../validation/Login");
 const RegisterUserValidation = require("../validation/RegisterUser");
+const ResetPasswordValidation = require("../validation/ResetPassword");
+const ForgotPasswordValidation = require("../validation/ForgotPassword");
+
+// import Controllers
 const AuthController = require("../Controllers/AuthController");
 
 router.post("/login", LoginValidation, AuthController.login);
 router.post("/register", RegisterUserValidation, AuthController.register);
-router.get("/forgot", AuthController.forgot);
-router.get("/reset", AuthController.reset);
+router.post("/forgot", ForgotPasswordValidation, AuthController.forgot);
+router.post("/reset", ResetPasswordValidation, AuthController.reset);
 router.post("/logout", requireAuth, verifyToken, AuthController.logout);
+router.get("/me", requireAuth, verifyToken, AuthController.me);
 
 module.exports = router;
